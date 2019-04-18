b0a9510f7b29dc5fb1406e96e9d55614585b7a70
Tolyan <moskalenkoup@gmail.com>
2019-04-18 20:54:21 +0300

    Test

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+let persone = {
+    name: 'John',
+    age: 30,
+    isMarryed: true
+};
+
+console.log(typeof(persone.age));
+
+
+let answer = +prompt('Есть ли вам 20?','Да');
+
+console.log(answer);
