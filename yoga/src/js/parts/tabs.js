c293477da21a454e99296e16fd4e733e5a955dc1
Tolyan <moskalenkoup@gmail.com>
2019-08-26 10:09:24 +0300

    WebPack

---
+++ yoga/src/js/parts/tabs.js
@@ -0,0 +1,48 @@
+function tabs() {
+    function tabs(tabContentSelector, tabHeaderSelector, tabBtnsSelector) {
+        let tabContent = document.querySelectorAll(tabContentSelector),
+            tabHeader = document.querySelector(tabHeaderSelector),
+            tabBtns = document.querySelectorAll(tabBtnsSelector);
+
+        function hideTabContent(a) {
+            for (let i = a; i < tabContent.length; i++) {
+                tabContent[i].classList.remove('show');
+                tabContent[i].classList.add('hide');
+            }
+        }
+
+        hideTabContent(1);
+
+        function showTabContent(b) {
+            if (tabContent[b].classList.contains('hide')) {
+                tabContent[b].classList.remove('hide');
+                tabContent[b].classList.add('show');
+            }
+        }
+
+        function hideAllTabs(c) {
+            if (tabContent[c].classList.contains('show')) {
+                hideTabContent(0);
+            }
+        }
+
+        tabHeader.addEventListener('click', function (event) {
+            let target = event.target;
+            if (target && target.classList.contains('info-header-tab')) {
+                for (let i = 0; i < tabBtns.length; i++) {
+                    if (target === tabBtns[i] && !tabContent[i].classList.contains('show')) {
+                        hideTabContent(0);
+                        showTabContent(i);
+                        break;
+                    } else {
+                        hideAllTabs(i);
+                    }
+                }
+            }
+        });
+    };
+
+    tabs('.info-tabcontent', '.info-header', '.info-header-tab');
+}
+
+module.exports = tabs;
